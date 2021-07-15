de2c8f578339d8edb0e50aaf5099d18f28eca652
Annii <anish.1903.kmr@gmail.com>
2021-07-15 20:59:14 +0530

    added method to get available meme data

--- Meme_Stream/meme_app/views.py
+++ Meme_Stream/meme_app/views.py
@@ -8,14 +8,18 @@ def home(request):
 
 
 def openForm(request):
+    memeData = models.MemeInfo.objects.all()
+    print(memeData[2].caption)
     return render(request, 'meme_app/submitMeme.html')
 
 def submitMeme(request):
     memerName = request.POST.get('memerName')
     caption = request.POST.get('caption')
     memeURL = request.POST.get('memeURL')
-    print(memerName, caption, memeURL)
+    # print(memerName, caption, memeURL)
 
     models.MemeInfo.objects.create(nameOfMemeOwner=memerName, caption=caption, memeUrl=memeURL)
+    # memeData = models.MemeInfo.objects.all()
+    # print(memeData)
 
     return render(request, 'meme_app/submitMeme.html')
