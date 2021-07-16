e3e4cfb55acb865dd5a73a9f44f55320211ca761
Annii <anish.1903.kmr@gmail.com>
2021-07-16 17:56:25 +0530

    Added edit button, made form for editable fields and added memeid's in stream page

--- Meme_Stream/meme_app/views.py
+++ Meme_Stream/meme_app/views.py
@@ -5,43 +5,32 @@ from . import models
 # Create your views here.
 def home(request):
     memeData = models.MemeInfo.objects.all()
-    # data = []
-    for memers in memeData:
-        # name = memers.nameOfMemeOwner
-        # caption = memers.caption
-        # imgUrl = memers.memeUrl
-        print(memers.nameOfMemeOwner)
-        print(memers.caption)
-        print(memers.memeUrl)
-        # data.append((name, caption, imgUrl))
-    # print(data)
-    # stream_data = {
-        # 'data': data,
-    # }
-        
-    # print(memeData[2].caption)
-    # print(memeData)
+    date = models.MemeInfo.objects.order_by('-created')
+    # a = models.MemeInfo.objects.get(id=29)
+    print(date)
+
     return render(request, 'base.html')
+
+def openEditForm(request):
+    return render(request, 'meme_app/editMeme.html')
+
 
 # for streaming the posted memes
 def stream(request):
-    # memeData = models.MemeInfo.objects.all()
-    # print(memeData[2].caption)
-    memeData = models.MemeInfo.objects.all()
+    memeData = models.MemeInfo.objects.order_by('-created')
     data = []
     for memers in memeData:
         name = memers.nameOfMemeOwner
         caption = memers.caption
         imgUrl = memers.memeUrl
-        # print(memers.nameOfMemeOwner)
-        # print(memers.caption)
-        # print(memers.memeUrl)
-        data.append((name, caption, imgUrl))
+        memeId = memers.id
+        print(memers.id)
+        data.append((name, caption, imgUrl, memeId))
     # print(data)
     stream_data = {
         'data': data,
     }
-    print(stream_data['data'])
+    # print(stream_data['data'])
     return render(request, 'meme_app/streamPage.html', stream_data)
 
 # Only for opening the submit form
@@ -56,6 +45,7 @@ def submitMeme(request):
 
     if request.POST.get('memerName') and request.POST.get('memeURL'):
         models.MemeInfo.objects.create(nameOfMemeOwner=memerName, caption=caption, memeUrl=memeURL)
+    # models.MemeInfo.objects.create(nameOfMemeOwner=memerName, caption=caption, memeUrl=memeURL)
 
 
 
@@ -63,3 +53,6 @@ def submitMeme(request):
     # print(memeData)
 
     return render(request, 'meme_app/submitMeme.html')
+
+def editMeme(request):
+    return render(request, 'meme_app/streamPage.html')
